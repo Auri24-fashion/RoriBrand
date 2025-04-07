155f899d16f72ba428a1a17e25a435d2b42de753
Auri24-fashion <aurora.cappai24@gmail.com>
2025-04-07 07:07:04 +0000

    java 2

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-// script.js
 
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Benvenuto nel portfolio di Aurora Cappai!");
-
-    // Esempio: evidenzia la sezione attiva al click nel menu
-    const links = document.querySelectorAll("nav a");
-
-    links.forEach(link => {
-        link.addEventListener("click", function () {
-            links.forEach(l => l.classList.remove("attivo"));
-            this.classList.add("attivo");
-        });
-    });
-});
 
