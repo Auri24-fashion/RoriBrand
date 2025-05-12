66f9933698c231df985717cf35f056c5ccb79094
Auri24-fashion <aurora.cappai24@gmail.com>
2025-05-12 08:58:10 +0000

    java 10

--- script.js
+++ script.js
@@ -102,5 +102,20 @@
         });
     });
 });
+document.querySelectorAll('.carousel').forEach(carousel => {
+    const images = carousel.querySelector('.carousel-images');
+    const imgs = images.querySelectorAll('img');
+    let index = 0;
+
+    carousel.querySelector('.next').addEventListener('click', () => {
+        index = (index + 1) % imgs.length;
+        images.style.transform = `translateX(-${index * 100}%)`;
+    });
+
+    carousel.querySelector('.prev').addEventListener('click', () => {
+        index = (index - 1 + imgs.length) % imgs.length;
+        images.style.transform = `translateX(-${index * 100}%)`;
+    });
+});
 
 
